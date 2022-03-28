0b726eef14e391db8ac22d1dd301ee26e97ca072
Mürüvet <muruvvetyoluk@gmail.com>
2022-03-28 10:39:11 +0300

    Ders-3 Tamamlandı

---
+++ ders3/ders3.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ders-3</title>
+</head>
+
+<body>
+
+<?php
+  /* Uygulama Ödevi
+ Kuyruk ve yığın veri yapılarının php üzerinde dizileri
+  kullanılarak örneklendirilmesini gerçekleştiriniz.*/
+
+  /* Uygulama : Rastgele üretilen sayı 
+  50'den büyükse
+    Geçtiniz:  Notunuz: xx
+  50'den küçükse
+    Kaldınız: Notunuz: xx
+    Uygulamasını gerçekleştiriniz.  */
+   $rastgele_Sayi = rand (10, 100);
+   if ($rastgele_Sayi >= 50)
+    {
+       echo "Geçtiniz - Notunuz : $ogr_notu ";
+   }
+    else
+   {
+       echo "Kaldınız - Notunuz : $ogr_notu ";
+   } 
+
+
+
+
+
+
+
+
+
+?>
+    
+</body>
+</html>
